5412cf8d0504d0cbf923d89ba8755ac48d36d7b5
CoderGirl <angi.adema@gmail.com>
2023-08-04 16:35:40 -0600

    added the seeds data to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -9,4 +9,12 @@ VALUES ("Gone With the Wind"),
 
 INSERT INTO reviews (movie_id, review)
 VALUES (1, "This movie was a great historical account!"),
-        
+       (2, "This is my favorite movie!"),
+       (1, "This movie was too long!"),
+       (3, "Odd sci-fi take on the world."),
+       (4, "Can't wait to see the rest of the terminator movies!"),
+       (2, "Uncle Buck is a good clean and fun movie."),
+       (5, "Jim Carey plays a great part in this movie."),
+       (5, "Love The Mask!"),
+       (6, "Liar Liar is a must see!"),
+       (7, "Prepare to get PTSD from The Notebook");
