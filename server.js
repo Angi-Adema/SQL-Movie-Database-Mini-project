bf5c74dbdd8959027b3e5800ef01d5c8b48e7665
CoderGirl <angi.adema@gmail.com>
2023-08-04 17:02:28 -0600

    added the get route for all movie names to server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'movie_db'
+    },
+    console.log(`Connected to the movie_db database.`)
+);
+
+app.get('/api/movies', (req, res) => {
+    const sql = 'SELECT id, movie_name AS title FROM movies';
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success!',
+            data: rows
+        });
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
