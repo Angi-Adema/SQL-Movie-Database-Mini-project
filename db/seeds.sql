ce2708208ba93a54768e4c63bb24a4cef648488a
CoderGirl <angi.adema@gmail.com>
2023-08-04 16:31:28 -0600

    updated the schema.sql file with the foreign key and references details

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO movies (movie_name)
+VALUES ("Gone With the Wind"),
+       ("Uncle Buck"),
+       ("Dune"),
+       ("Terminator 2 Judgment Day"),
+       ("The Mask"),
+       ("Liar Liar"),
+       ("The Notebook");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "This movie was a great historical account!"),
+        
