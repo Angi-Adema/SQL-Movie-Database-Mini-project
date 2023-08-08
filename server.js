3ff9f927e436ea5eb786f427fd4df7df8a74f474
CoderGirl <angi.adema@gmail.com>
2023-08-07 18:08:27 -0600

    added the get route to retrieve all reviews, put route to update a review, route to allow for a partial movie name to search for movie names and route to pull up one movie name

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 
 const db = mysql.createConnection(
     {
@@ -36,11 +36,12 @@ app.get('/api/movies', (req, res) => {
 
 // Get request to render one movie.
 app.get('/api/movies/:id', (req, res) => {
-    const sql = 'SELECT id = ?, movie_name FROM movies';
+    // Can write this either way (storing data in the const variable as seen commented out below or through a template literal).
 
-    const params = [req.params.id];
+    // const sql = 'SELECT movie_name FROM movies WHERE id = ?';
+    // const params = [req.params.id];
 
-    db.query(sql, params, (err, result) => {
+    db.query(`SELECT * FROM movies where id = ${req.params.id};`, (err, result) => {
         if (err) {
             res.status(500).json({ error: err.message });
             return;
@@ -52,8 +53,23 @@ app.get('/api/movies/:id', (req, res) => {
     });
 });
 
+// Search by movie name by entering a partial movie name and return the result in descending order.
+app.get('/api/reviews/:movie', (req, res)=>{
+    db.query(`SELECT reviews.id, movies.movie_name AS title, reviews.review FROM reviews 
+    LEFT JOIN movies ON reviews.movie_id = movies.id WHERE movie_name LIKE '%${req.params.movie}%' ORDER BY reviews.id DESC; `, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success!',
+            data: result
+        });
+    });
+})
+
 // Post request to add a movie to the database.
-app.post('/api/add', ({ body }, res) => {
+app.post('/api/movies', ({ body }, res) => {
     const sql = 'INSERT INTO movies (movie_name) VALUE (?)';
 
     const nameParam = [body.movie_name]; 
@@ -114,9 +130,46 @@ app.delete('/api/delete/:id', (req, res) => {
             });
         }
     });
-
 });
 
+// Get route to get all the reviews in the database.
+app.get('/api/reviews', (req, res) => {
+    const sql = 'SELECT * FROM reviews';
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'Success!',
+            data: rows
+        });
+    });
+});
+
+// Put route to update a review in the database.
+app.put('/api/reviews/:id', (req, res) => {
+    const sql = 'UPDATE reviews SET review = ? WHERE id = ?';
+    
+    const params = [req.body.review, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Review not found!'
+            });
+        } else {
+            res.json({
+                message: 'Success!',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
+    });
+});
 
 app.use((req, res) => {
     res.status(404).end();
